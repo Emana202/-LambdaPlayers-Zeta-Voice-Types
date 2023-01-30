5c5aba6f6ac71b4058083e4787df84cd35e333f5
ErmaaS-41 <emana202@yandex.ru>
2023-01-30 17:11:27 +0600

    Updated to Lambda's new external addon system

--- lua/lambdaplayers/extaddon/shared/lambdaplayers_zetavoicetypes.lua
+++ lua/lambdaplayers/extaddon/shared/lambdaplayers_zetavoicetypes.lua
@@ -1,22 +1,19 @@
-hook.Add( "LambdaOnVoiceTypesRegistered", "LambdaZetaVoiceTypes_RegisterVoiceTypes", function()
-	LambdaRegisterVoiceType( "witnesskill", "lambdaplayers/vo/witnesskill", "These are voice lines that play when a Lambda Player sees someone get killed." )
-    LambdaRegisterVoiceType( "killassist", "lambdaplayers/vo/killassist", "These are voice lines that play when someone else kills Lambda Player's current enemy." )
-    LambdaRegisterVoiceType( "fall", "lambdaplayers/vo/fall", "These are voice lines that play when a Lambda Player starts falling from deadly distance." )
-end)
+LambdaRegisterVoiceType( "fall", "lambdaplayers/vo/fall", "These are voice lines that play when a Lambda Player starts falling from deadly distance." )
+LambdaRegisterVoiceType( "assist", "lambdaplayers/vo/assist", "These are voice lines that play when someone else kills Lambda Player's current enemy." )
+LambdaRegisterVoiceType( "witness", "lambdaplayers/vo/witness", "These are voice lines that play when a Lambda Player sees someone get killed." )
 
 if ( CLIENT ) then return end
 
-local realDamage = GetConVar( "lambdaplayers_lambda_realisticfalldamage" )
-local TraceHull = util.TraceHull
-local fallTrTbl = {}
+local IsValid = IsValid
 local math_max = math.max
 local random = math.random
 local Rand = math.Rand
-local IsValid = IsValid
-
-local fallPathCvar
-local witnessPathCvar
-local assistPathCvar
+local TraceHull = util.TraceHull
+local fallTrTbl = {}
+local realDamage = GetConVar( "lambdaplayers_lambda_realisticfalldamage" )
+local fallDir = GetConVar( "lambdaplayers_voice_falldir" )
+local assistDir = GetConVar( "lambdaplayers_voice_assistdir" )
+local witnessDir = GetConVar( "lambdaplayers_voice_witnessdir" )
 
 hook.Add( "LambdaOnLeaveGround", "LambdaZetaVoiceTypes_OnLeaveGround", function( self, ground )
     local deathDist = 800
@@ -28,12 +25,10 @@ hook.Add( "LambdaOnLeaveGround", "LambdaZetaVoiceTypes_OnLeaveGround", function(
     fallTrTbl.mins = self:OBBMins()
     fallTrTbl.maxs = self:OBBMaxs()
     if TraceHull( fallTrTbl ).Hit then return end
-    	
+
 	fallTrTbl.endpos = ( fallTrTbl.start - self:GetUp() * 32756 )
 	if TraceHull( fallTrTbl ).HitPos:IsUnderwater() then return end
-
-    fallPathCvar = fallPathCvar or GetConVar( "lambdaplayers_voice_falldir" )
-    self:PlaySoundFile( ( fallPathCvar:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "fall" ) ), true )
+    self:PlaySoundFile( ( fallDir:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "fall" ) ), true )
 end )
 
 hook.Add( "LambdaOnOtherKilled", "LambdaZetaVoiceTypes_OnOtherKilled", function( self, victim, dmginfo )
@@ -51,18 +46,15 @@ hook.Add( "LambdaOnOtherKilled", "LambdaZetaVoiceTypes_OnOtherKilled", function(
             if victim != curEnemy or random( 1, 100 ) > self:GetVoiceChance() then return end
             if !attacker:IsPlayer() and !attacker:IsNPC() and !attacker:IsNextBot() or !self:CanSee( attacker ) then return end
 
+            self:LookTo( attacker, 1 )
             self:SimpleTimer( Rand( 0.1, 1.0 ), function()
                 if !IsValid( attacker ) then return end
-                assistPathCvar = assistPathCvar or GetConVar( "lambdaplayers_voice_killassistdir" )
-                self:PlaySoundFile( ( assistPathCvar:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "killassist" ) ), true )
+                self:PlaySoundFile( ( assistDir:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "assist" ) ), true )
             end )
-        else
-            if random( 1, 10 ) != 1 or !self:IsInRange( victimPos, 2000 ) then return end
+        elseif random( 1, 10 ) == 1 and self:IsInRange( victimPos, 2000 ) then
             self:LookTo( victimPos, random( 1, 3 ) )
-
             self:SimpleTimer( Rand( 0.1, 1.0 ), function()
-                witnessPathCvar = witnessPathCvar or GetConVar( "lambdaplayers_voice_witnesskilldir" )
-                self:PlaySoundFile( ( witnessPathCvar:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "witnesskill" ) ), true )
+                self:PlaySoundFile( ( witnessDir:GetString() == "randomengine" and self:GetRandomSound() or self:GetVoiceLine( "witness" ) ), true )
             end )
         end
     end )
